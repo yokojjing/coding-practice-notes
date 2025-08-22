118313969ac1a6e2a1af44a85928442e7180d7c3
yokomon <yokojjing@gmail.com>
2025-08-22 22:31:17 +0900

    Time: 0 ms (100%), Space: 17.9 MB (39.92%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,10 +1,13 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        value = {'M':1000,'D':500,'C':100,'L':50,'X':10,'V':5,'I':1}
+        values = {'M':1000,'D':500,'C':100,'L':50,'X':10,'V':5,'I':1}
+        pre_value = 0
         total = 0
-        for i in range(len(s)):
-            if i < len(s)-1 and value[s[i]] < value[s[i+1]]:
-                total -= value[s[i]]
+        for char in reversed(s):
+            value = values[char]
+            if value < pre_value:
+                total -= value
             else:
-                total += value[s[i]]
+                total += value
+            pre_value = value
         return total

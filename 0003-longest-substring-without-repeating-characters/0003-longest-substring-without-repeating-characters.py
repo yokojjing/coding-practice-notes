8f72d0531b160f7af3dc3bc51702167ce32db1c9
yokomon <yokojjing@gmail.com>
2025-08-23 21:46:28 +0900

    Time: 15 ms (80.62%), Space: 18 MB (12.12%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,13 +4,12 @@ class Solution:
             return 0
         if len(s) == 1:
             return 1
-        lookup = set()
-        left = 0
+        char_index = {}
         max_leng = 0
+        left = 0
         for right in range(len(s)):
-            while s[right] in lookup:
-                lookup.remove(s[left])
-                left += 1
-            lookup.add(s[right])
+            if s[right] in char_index and char_index[s[right]] >= left:
+                left = char_index[s[right]] +1
+            char_index[s[right]] = right
             max_leng = max(max_leng,right-left+1)
         return max_leng

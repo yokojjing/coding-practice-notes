d048048b31f0c34d58413ed4dcbbaf0bb7a4a45b
yokomon <yokojjing@gmail.com>
2025-08-24 15:51:15 +0900

    Time: 2874 ms (21.54%), Space: 17.7 MB (83.37%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,9 +1,9 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         if len(s) == 1:
-            s
-        max_leng = s[0]
-        for i in range(0,len(s)-1):
+            return s
+        max_leng = ''
+        for i in range(len(s)):
             for j in range(i+1):
                 left = j
                 right = len(s)-1-i+j
@@ -17,4 +17,3 @@ class Solution:
                 if sign:
                     max_leng = s[j:len(s)-i+j]
                     return max_leng
-        return max_leng

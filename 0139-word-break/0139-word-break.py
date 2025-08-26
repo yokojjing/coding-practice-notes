4bc66d2cca6d79ba08a40cdf32fcc8fc2539a99c
yokomon <yokojjing@gmail.com>
2025-08-27 00:10:31 +0900

    Time: 3 ms (69.25%), Space: 18 MB (25.13%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,13 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        n = len(s)
+        match_to = [False]*(n+1)
+        match_to[0] = True
+        for i in range(n+1):
+            for j in range(i):
+                if match_to[j] and s[j:i] in word_set:
+                    match_to[i] = True
+                    break
+        return match_to[n]
+                

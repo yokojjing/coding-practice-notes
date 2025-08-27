7baffdee99392865f7462e1e4e4f00aa0906ba33
yokomon <yokojjing@gmail.com>
2025-08-27 17:21:01 +0900

    Time: 7 ms (20.48%), Space: 23.3 MB (38.35%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -10,35 +10,23 @@ class Solution:
         """
         if not head.next:
             return head
-        slow = head
-        fast = head
-        while fast and fast.next:
+        slow = fast = head
+        while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
-        mid = slow
-        prev = slow
-        cur = slow.next
-        while cur:
-            temp = cur.next
-            cur.next = prev
-            prev = cur
-            cur = temp
-        backwards = prev
-        back_next = prev.next
-        forwards = head
-        for_next = head.next
-        while forwards != mid:
-            forwards.next = backwards
-            if backwards == mid:
-                backwards.next = None
-            else:
-                backwards.next = for_next
-            temp = for_next.next
-            forwards = for_next
-            for_next = temp
-            backwards = back_next
-            if back_next:
-                back_next = back_next.next
-        if forwards.next:
-            forwards.next = None
+        second = slow.next
+        slow.next = None
+        prev = None
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+
+        first,second = head,prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first,second = temp1,temp2
         return head
